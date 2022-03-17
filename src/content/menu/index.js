610f36fbae5441eb568bba622b206b6315be8291
Kalbarczyk Kamil <kkalbarczyk@networks.pl>
2022-03-17 21:42:25 +0100

    add data burgers from api

--- src/content/menu/index.js
+++ src/content/menu/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { PageWrapper } from "../../common/page-wrapper";
 import * as React from 'react';
 import Table from '@mui/material/Table';
@@ -9,6 +10,17 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 export const Menu = () => {
+    const [ burgers, setBurgers ] = useState([]);
+
+    useEffect(()=>{
+        fetch('https://rest-api-b6410.firebaseio.com/burgers.json')
+            .then((response)=> response.json())
+            .then(data => {
+                const formattedData = Object.keys(data).map((key) => ({id: key, ...data[key]}))
+                setBurgers(formattedData);
+            })
+    },[])
+
     return <PageWrapper title="Menu">
             <TableContainer component={Paper}>
       <Table>
@@ -20,6 +32,13 @@ export const Menu = () => {
           </TableRow>
         </TableHead>
         <TableBody>
+          {burgers.map(burger => (
+             <TableRow key={burger.id}>
+               <TableCell>{burger.name}</TableCell>
+               <TableCell align="right">{burger.ingredients}</TableCell>
+               <TableCell align="right">{burger.price}</TableCell>
+           </TableRow>
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
